da4335bd2b703b7c8e11bc71f78117d15263bebc
andras.sik <andras.sik@dfff75b4-b895-11dd-b612-3927438af0ba>
2008-11-24 23:29:15 +0000

    Multi thread socket handling implemented (max connections are 100 atm). The ClientEntity class represents a client thread.
    TODO: maintenance of connection count!!!

---
+++ ClientEntity.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package shareonserver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/**
+ *
+ * @author Bandita
+ */
+
+// class to represents clients to connect to the ShareOn server
+class ClientEntity implements Runnable
+        {
+        Socket sServer;                 //socket to communicate with the client
+        String sFromClient;             //message received from the client
+        PrintWriter pwOut = null;       //PrintWriter to wrtite
+        BufferedReader buffIn = null;   //BufferedReader to read
+        String sLine;
+
+        //save the socket in the constructor
+        ClientEntity(Socket server) { this.sServer=server; }
+
+        public void run ()
+            {
+            //we just send back what we have received
+            try 
+                {
+                pwOut = new PrintWriter(sServer.getOutputStream(), true);
+                buffIn = new BufferedReader(new InputStreamReader(sServer.getInputStream()));
+                
+                while(!(sLine = buffIn.readLine()).equals("logout"))
+                    {
+                    pwOut.println(sLine);
+                    }
+                
+                pwOut.close();
+                buffIn.close();
+                sServer.close();
+                }
+            catch (IOException e)
+                {
+                System.err.println("Exception occured: " + e.toString());
+                }
+            }
+        }
